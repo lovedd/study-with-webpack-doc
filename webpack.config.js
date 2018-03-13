1bd8bbd26936620c4463d2a4724e93781c040587
liuliu <liuliu@MacBook-Pro-3.local>
2018-03-13 19:42:16 +0800

    加载数据

--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,21 @@ module.exports = {
                 ]
             },
             {
-                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
                 ]
             }
         ]
